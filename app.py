2fc88f879ebf39d790a6c3e7918aeded31fc3cea
Zhepp <xefthynharry19@gmail.com>
2025-12-19 06:50:30 +0700

    yeah

--- app.py
+++ app.py
@@ -15,6 +15,13 @@ def get_video():
         'quiet': True,
         'no_warnings': True,
         'format': 'best',
+        'http_headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-us,en;q=0.5',
+            'Sec-Fetch-Mode': 'navigate',
+            'cookiesfrombrowser': ('chrome',),
+        }
     }
     
     try:
